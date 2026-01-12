6bf2bfe1115b28b4de2a7265fd0c780788b002f7
v.borodulin <v.borodulin@tbank.ru>
2026-01-12 16:14:40 +0300

    __init__

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "todolist"
+
+pluginManagement {
+    repositories {
+        maven("https://artifactory.tcsbank.ru/artifactory/maven-all")
+    }
+}
+
+buildscript {
+    repositories {
+        maven("https://artifactory.tcsbank.ru/artifactory/maven-all")
+    }
+}
